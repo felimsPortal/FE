a2054a518ef48d3c34cba6fce05d2ff2ecb89ef9
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2024-05-05 20:32:48 +0200

    New Branch

--- src/app/pages/portalUnsubscribed/page.jsx
+++ src/app/pages/portalUnsubscribed/page.jsx
@@ -1,8 +1,30 @@
+"use client";
 import Link from "next/link";
 import Image from "next/image";
 import { FaHome, FaFilter, FaSearch, FaHeart } from "react-icons/fa";
+import { useState, useRef, useLayoutEffect } from "react";
 
 const Portal = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const containerRef = useRef(null);
+  const inputRef = useRef(null);
+
+  const handleClickOutside = (e) => {
+    if (
+      containerRef.current &&
+      !containerRef.current.contains(e.target) &&
+      inputRef.current &&
+      !inputRef.current.contains(e.target)
+    ) {
+      setIsOpen(false);
+    }
+  };
+
+  useLayoutEffect(() => {
+    window.addEventListener("click", handleClickOutside);
+
+    return () => window.removeEventListener("click", handleClickOutside);
+  }, []);
   return (
     <div className="h-screen w-screen overflow-hidden">
       <div className="w-full h-24 bg-green-700 flex justify-evenly items-center">
@@ -20,20 +42,44 @@ const Portal = () => {
           </button>
         </Link>
       </div>
-      <div className="h-full w-28 bg-red-800">
-        <div className="h-2/3 flex flex-col items-center justify-evenly">
-          <Link href="/portalSubscribed">
-            <FaHome size={80} />
-          </Link>
-          <Link href="/portalSubscribed">
-            <FaFilter size={80} />
-          </Link>
-          <Link href="/portalSubscribed">
-            <FaSearch size={80} />
-          </Link>
-          <Link href="/portalSubscribed">
-            <FaHeart size={80} />
-          </Link>
+      <div ref={containerRef}>
+        <div className="h-20 w-full bg-red-800 items-center">
+          <div className="h-full w-full items-center flex justify-between px-10">
+            <div className="group w-full flex justify-between items-center">
+              <div className="relative w-1/2">
+                <div className=" flex" onClick={() => setIsOpen(!isOpen)}>
+                  <div>
+                    <FaSearch size={40} />
+                  </div>
+                  {isOpen && (
+                    <div className="ml-10 bg-white shadow-md rounded-md w-3/5 z-50 ">
+                      <input
+                        ref={inputRef}
+                        type="text"
+                        placeholder="Search felims"
+                        className="w-full p-2 border-gray-300 rounded-md focus:outline-none"
+                      />
+                    </div>
+                  )}
+                </div>
+              </div>
+              <div className="absolute invisible group-hover:visible bg-gray-800 text-white px-2 py-1 rounded-md bottom-full left-0  text-center">
+                Search
+              </div>
+            </div>
+
+            <div className="flex w-1/2 justify-evenly">
+              <Link href="/portalSubscribed">
+                <FaHome size={40} />
+              </Link>
+              <Link href="/portalSubscribed">
+                <FaFilter size={40} />
+              </Link>
+              <Link href="/portalSubscribed">
+                <FaHeart size={40} />
+              </Link>
+            </div>
+          </div>
         </div>
       </div>
     </div>
