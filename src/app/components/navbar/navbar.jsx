66d4d9cc2fd4f48fb7bada9a4a5824d0e07abeae
Ashraf Abdelrahman <ashraf.ak.rahman@gmail.com>
2024-07-28 13:42:09 +0200

    navbar dropdown started

--- src/app/components/navbar/navbar.jsx
+++ src/app/components/navbar/navbar.jsx
@@ -1,9 +1,48 @@
 "use client";
 import Image from "next/image";
 import { UserAuth } from "../../context/AuthContext";
+import { Odibee_Sans } from "next/font/google";
+import { useState } from "react";
+
+const Odibee = Odibee_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 function Navbar() {
   const { user, logOut } = UserAuth();
+  const [isSearching, setIsSearching] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
+  const navLinks = [
+    { name: "Home", href: "/pages/portalUnsubscribed" },
+    { name: "TV Shows", href: "#" },
+    { name: "Movies", href: "#" },
+    { name: "Bookmarks", href: "#" },
+  ];
+
+  const handleDropDownToggle = () => {
+    setIsDropdownOpen(!isDropdownOpen);
+  };
+
+  const handleSearchClick = () => {
+    setIsSearching(true);
+  };
+
+  const handleInputChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const handleInputBlur = () => {
+    setIsSearching(false);
+  };
+
+  const handleSearchSubmit = (e) => {
+    e.preventDefault();
+    console.log("searching for:", searchQuery);
+    setIsSearching(false);
+  };
 
   const handleSignOut = async () => {
     try {
@@ -15,48 +54,132 @@ function Navbar() {
     }
   };
 
-  const navLinks = [
-    { name: "Home", href: "/pages/portalUnsubscribed" },
-    { name: "TV Shows", href: "#" },
-    { name: "Movies", href: "#" },
-    { name: "My bookmarks", href: "#" },
-    { name: "Sign Out", href: handleSignOut },
+  const dropDownLinks = [
+    { name: "Profile", href: "/pages/profile" },
+    { name: "Donation info", href: "/donation" },
   ];
   return (
-    <div className="fixed w-screen h-1/6 flex items-center border-b-2 border-solid border-red-800  bg-black z-10">
+    <div
+      className="fixed w-screen h-1/6 bg-transparent z-10"
+      style={{ backgroundColor: "transparent" }}
+    >
       {user ? (
-        <div>
-          <Image
-            className=""
-            src="/logoOfficial.png"
-            alt="logo"
-            width={140}
-            height={150}
-          />
+        <div
+          className="fixed w-screen h-1/6 flex items-center z-10 px-16 bg-transparent "
+          style={{ backgroundColor: "transparent" }}
+        >
+          <div className="w-1/5 h-full flex items-end justify-between px-4">
+            <Image
+              className="mb-1"
+              src="/Logo2.png"
+              alt="logo"
+              width={70}
+              height={150}
+            />
+            <div className={Odibee.className}>
+              <h1 className="text-7xl">FELiMS</h1>
+            </div>
+          </div>
 
-          <div className="ml-10 flex items-baseline justify-between">
+          <div className="w-3/5 h-full flex items-end justify-evenly ">
             {navLinks.map((link, index) => (
-              <a key={index} href={link.link}>
+              <a
+                className="transform transition-transform duration-300 hover:scale-150"
+                key={index}
+                href={link.link}
+              >
                 {link.name}
               </a>
             ))}
-            <button
+            {isSearching ? (
+              <div className="">
+                <form onSubmit={handleSearchSubmit}>
+                  <input
+                    type="text"
+                    className="w-56 h-8 border border-gray-300 rounded-3xl focus:outline-none"
+                    placeholder="Search felims"
+                    value={searchQuery}
+                    onChange={handleInputChange}
+                    onBlur={handleInputBlur}
+                    autoFocus
+                  />
+                </form>
+              </div>
+            ) : (
+              <button
+                className="transform transition-transform duration-300 hover:scale-150"
+                onClick={handleSearchClick}
+              >
+                Search
+              </button>
+            )}
+          </div>
+
+          <div className="w-1/5 h-full flex justify-between items-end px-28">
+            <div className="relative">
+              <button className=" z10 block rounded-md p2 focus:outline-none">
+                <Image
+                  className="mr-1"
+                  src="/dropDown.png"
+                  alt="dropDown"
+                  width={70}
+                  height={150}
+                  onClick={handleDropDownToggle}
+                />
+              </button>
+              {isDropdownOpen && (
+                <div className="absolute top-2 rounded-sm w-48 z-50 flex flex-col">
+                  <div className="bg-black opacity-40 absolute h-full w-5/6 rounded-2xl -z-10"></div>
+                  <div className="w-5/6 h-full flex flex-col text-center">
+                    {dropDownLinks.map((link, index) => (
+                      <a key={index} href={link.href}>
+                        {link.name}
+                      </a>
+                    ))}
+                    <button className="" onClick={handleSignOut}>
+                      Sign Out
+                    </button>
+                  </div>
+                </div>
+              )}
+            </div>
+
+            {/* <Image
+              className="mr-1"
+              src="/dropDown.png"
+              alt="dropDown"
+              width={20}
+              height={150}
+            /> */}
+            <Image
+              className="border-2 border-solid border-red-800 rounded-md"
+              src="/Profile.jpg"
+              alt="Profile Water Melon"
+              width={80}
+              height={150}
+            />
+            {/* <button
               className="bg-gray-900 w-40 h-12 rounded-lg shadow-sm shadow-gray-500 cursor-pointer z-50 border border-red-500"
               onClick={handleSignOut}
             >
               Sign Out
-            </button>
+            </button> */}
           </div>
         </div>
       ) : (
-        <div className="w-full h-full flex items-center justify-center">
-          <Image
-            className=""
-            src="/logoOfficial.png"
-            alt="logo"
-            width={140}
-            height={150}
-          />
+        <div className="fixed w-screen h-1/6 flex items-center justify-center border-b-2 border-solid border-red-800  bg-black z-10 ">
+          <div className="w-1/5 h-full flex items-end justify-between px-8">
+            <Image
+              className="mb-2"
+              src="/Logo2.png"
+              alt="logo"
+              width={75}
+              height={150}
+            />
+            <div className={Odibee.className}>
+              <h1 className="text-7xl mb-2">FELiMS</h1>
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/app/pages/portalUnsubscribed/page.jsx
+++ src/app/pages/portalUnsubscribed/page.jsx
@@ -1,12 +1,65 @@
 "use client";
 import React from "react";
 import Navbar from "@/app/components/navbar/navbar";
+import { Poiret_One } from "next/font/google";
+import Image from "next/image";
+
+const Poiret = Poiret_One({
+  weight: "400",
+  subsets: ["latin"],
+});
 
 const Portal = () => {
   return (
-    <div className="w-screen h-screen">
-      <div className="h-1/6">
+    <div className="w-screen h-screen ">
+      <div className="">
         <Navbar />
+      </div>
+      <div className=" w-full h-5/6 flex items-center">
+        <div className="w-full h-full px-14 py-10">
+          <video
+            className="w-full h-full object-cover rounded-3xl "
+            src="/homePage.mp4
+        "
+            autoPlay
+            loop
+            muted
+          />
+        </div>
+
+        <div className="absolute h-2/3 w-1/4 flex flex-col items-center justify-center ml-24 mt-48  px-10">
+          <div className="w-full h-1/3 ">
+            <h1
+              className={`text-7xl font-extrabold text-white mt-10 ${Poiret.className}`}
+            >
+              MOVIE TITLE
+            </h1>
+          </div>
+          <div className="w-full h-1/3">
+            <h1 className="mt-5">
+              Synopsis: Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum has been the industry standard
+              dummy text ever since the 1500s, when an unknown printer took a
+              galley of type and
+            </h1>
+          </div>
+          <div className="flex w-full h-1/3">
+            <Image
+              width={55}
+              height={15}
+              className="h-1/4"
+              src="/4k.png"
+              alt="4K"
+            />
+            <Image
+              width={475}
+              height={15}
+              className="h-1/4 ml-5"
+              src="/details.png"
+              alt="4K"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
